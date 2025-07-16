0b7d985895a9b6b1106650362d0c7c7fc83fbbff
Gabriel Carmona <191816876+Gcarmnonapy7@users.noreply.github.com>
2025-07-16 13:33:23 -0300

    adding matrices operations (add and multiplication)

--- src/linalg/matrix_ops.jl
+++ src/linalg/matrix_ops.jl
@@ -1,10 +1,17 @@
 #Matrix operations module from scratch
 
-function matrix_addition(A::Matrix{Float64}, B::Matrix{Float64})
+function matrix_addition(A::AbstractMatrix, B::AbstractMatrix)
     # Matrix addition
     if size(A) != size(B)
         error("Matrices A and B must have the same dimensions.")
-        return nothing
     end
     return A .+ B
+end
+
+function matrix_multiplication(A::AbstractMatrix,B::AbstractMatrix)
+    #Matrix multiplication 
+    if size(A,2) != size(B,1)
+        error("Number of columns in A must be equal to the number of rows in B.")
+    end
+    return A * B
 end
